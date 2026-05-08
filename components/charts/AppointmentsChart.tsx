0e863b6f65997b1fc76d417ac33a8eb4e228a1df
Ayush Vyas <s.ayushvyas3925@gmail.com>
2026-05-08 16:07:53 +0530

    fix: update Recharts minWidth and minHeight to 1 to bypass strict greater-than-zero check

--- components/charts/AppointmentsChart.tsx
+++ components/charts/AppointmentsChart.tsx
@@ -51,7 +51,7 @@ export function AppointmentsChart({ appointments }: Props) {
       </div>
       
       <div className="h-[250px] w-full">
-        <ResponsiveContainer width="100%" height="100%" minWidth={0}>
+        <ResponsiveContainer width="100%" height="100%" minWidth={1} minHeight={1}>
           <BarChart data={chartData} margin={{ top: 0, right: 0, left: -20, bottom: 0 }}>
             <CartesianGrid strokeDasharray="3 3" vertical={false} stroke="#F1F5F9" />
             <XAxis 
